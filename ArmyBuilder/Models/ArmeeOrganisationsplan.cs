f7e23994c26a6ebe1461f7241d00ff73d14013fc
Andreas Möller <exec223@hotmail.de>
2017-10-26 17:46:18 +0200

    added first models

---
+++ ArmyBuilder/Models/ArmeeOrganisationsplan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyBuilder.Models
+{
+    public class ArmeeOrganisationsplan
+    {
+        string Name { get; set; }
+        int Punkte { get; set; }
+        Armee Armee { get; set; }
+    }
+
+}
